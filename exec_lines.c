e8b95ff3eb55cb683dd52ad7ab3b82351c769091
alfonsete23 <alfonso.navarrog@um.es>
2022-10-19 15:08:04 +0200

    Mejora de exec_lines en proceso. - Solamente se lee la primera línea, buscar motivo y solucionar

--- exec_lines.c
+++ exec_lines.c
@@ -9,39 +9,62 @@
 #define BLOCK_SIZE 16
 #define LINE_SIZE 128
 
-// Función para ejecutar una línea de comandos
-void ejecutar_linea(char *line)
-{
-    // Iniciar proceso hijo para procesar la línea leída
-    char *arr[LINE_SIZE];
-    int i;
-    char *p;
-    pid_t pid;
-    switch (pid = fork())
-    {
-    // Fork falla
-    case -1:
-        perror("fork()");
-        exit(EXIT_FAILURE);
-        break;
-    // Fork exitoso, procesamos la línea leída
-    case 0:
-        // Separar el contenido de la línea en un array para la llamada a execvp
-        i = 0;
-        p = strtok(line, " ");
-        while (p != NULL)
-        {
-            arr[i++] = p;
-            p = strtok(NULL, " ");
-        }
-        arr[i] = NULL;
-        execvp(arr[0], arr);
+int ejecutando = 0;
 
-    default:
-        // Actualizar buf con el resto del bloque leído
-        return;
-    }
+// Estructura para leer líneas de forma dinámica, con el tamaño de la línea duplicándose cada vez que se lee
+typedef struct
+{
+    char *dir;
+    size_t used;
+    size_t size;
+} Line;
+
+// Inicializar la estructura Line
+void initLine(Line *line)
+{
+    line->dir = calloc(sizeof(char) * LINE_SIZE, LINE_SIZE);
+    line->used = 0;
+    line->size = LINE_SIZE;
 }
+
+// Insertar los "num_read" primeros bytes de buf en la cadena guardada en la estructura Line
+int insertLine(Line *line, char *buf, int num_read)
+{
+    // Si el tamaño tras la adición supera al tamaño reservado, devolvemos el error y paramos la ejecución
+    if ((line->used + num_read) > line->size)
+    {
+        return -1;
+    }
+    // Copiamos lo leído en el buffer en la cadena
+    memcpy(line->dir + line->used, buf, num_read);
+    // Actualizar el tamaño de la cadena
+    line->used += num_read;
+    return 0;
+}
+
+// Devuelve la cadena almacenada
+char *getLine(Line *line)
+{
+    return line->dir;
+}
+
+// Devuelve el tamaño de la cadena
+size_t getSize(Line *line)
+{
+    return line->used;
+}
+
+// Liberar memoria
+void freeLine(Line *line)
+{
+    free(line->dir);
+    line->used = 0;
+    line->size = 0;
+}
+
+// Prototipos de funciones
+Line leer_linea(int fd);
+void ejecutar_linea(char *l);
 
 int main(int argc, char **argv)
 {
@@ -80,97 +103,45 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    
-
-    // TODO: Implementar lectura de líneas entrada estándar usando solamente read()
-    char *buf = malloc(BLOCK_SIZE);
-    char *aux = malloc(BLOCK_SIZE);
-    char *line = malloc(LINE_SIZE);
+    char *l;
+    Line line;
     int num_read, length = 0;
-    int line_executed;
     char *end;
-    int repeated = 0;
-    int restantes = numproc;
-
+    int line_size;
     int status;
+
     // Leer de la entrada estándar
-    while ((num_read = read(STDIN_FILENO, buf, BLOCK_SIZE)) > 0)
+    while (1)
     {
-        line_executed = 0;
-        repeated = 0;
-        // Si la línea supera la longitud de 128 bytes, error.
-        if ((length + num_read) > LINE_SIZE)
+        // Leer línea
+        line = leer_linea(STDIN_FILENO);
+        if ((line_size = getSize(&line)) == 0)
         {
-            fprintf(stderr, "Error. Tamaño de línea mayor que 128\n");
-            exit(EXIT_FAILURE);
+            freeLine(&line);
+            break;
         }
-
-        // Si en el bloque leído hay uno o más saltos de línea, reservamos lo leído de la siguiente y procesamos la línea terminada
-        while ((end = strchr(buf, '\n')))
+        l = getLine(&line);
+        if (ejecutando == numproc)
         {
-            if (!repeated)
+            if (wait(&status) == -1)
             {
-                memcpy(line + length, buf, end - buf);
-                repeated = 0;
-
-            }
-            // Comprobar que no hay más procesos activos de los que deben
-            if (restantes < 1)
-            {
-                if (wait(&status) == -1)
-                {
-                    perror("wait()");
-                    exit(EXIT_FAILURE);
-                }
-
-                restantes++;
+                perror("wait()");
+                exit(EXIT_FAILURE);
             }
 
-            restantes--;
-            ejecutar_linea(line);
-            length = 0;
-            num_read = num_read - (end - buf);
-            memcpy(aux, end + 1, num_read);
-            memset(buf, 0, BLOCK_SIZE);
-            memset(line, 0, LINE_SIZE);
-            memcpy(buf, aux, num_read);
-            if ((end = strchr(buf, '\n')))
+            if (WIFEXITED(status))
             {
-                repeated = 1;
-                num_read = end - buf;
+                ejecutando--;
             }
-            memcpy(line, buf, num_read);
-            length += num_read;
-            line_executed = 1;
         }
-        if (!line_executed)
-        {
-            // Añadir lo leído en el buffer a la línea que se está leyendo
-            memcpy(line + length, buf, num_read);
-            // Aumentar la longitud de la línea
-            length += num_read;
-        }
-    }
-
-    // Tratamiento de la última línea si la entrada no acaba con un salto de línea
-    if (restantes < 1)
-    {
-        if (wait(&status) == -1)
-        {
-            perror("wait()");
-            exit(EXIT_FAILURE);
-        }
-        restantes++;
-    }
-
-    if (line[0] != 0)
-    {
-        ejecutar_linea(line);
+        printf("%s", l);
+        // ejecutar_linea(l);
+        freeLine(&line);
     }
 
 
     // Espera de procesos que pueden seguir en ejecución
-    while (restantes < numproc)
+    while (ejecutando > 0)
     {
         if (wait(&status) == -1)
         {
@@ -180,12 +151,97 @@ int main(int argc, char **argv)
 
         if (WIFEXITED(status))
         {
-            restantes++;
+            ejecutando--;
         }
     }
+}
 
-    // Liberación de memoria
+// Esta función lee una línea del fichero representado por fd, en bloques de tamaño bufsize
+Line leer_linea(int fd)
+{
+    // Buffer de lectura
+    char *buf;
+    // Número de bytes leídos y número de bytes leídos que pertenecen a la línea que estamos leyendo
+    int num_read, read_in_line;
+    // Line es una especie de array dinámico que duplica su tamaño cada vez que lo leído supere al tamaño reservado en memoria
+    Line line;
+    // Puntero que apunta a la ocurrencia del carácter '\n'
+    char *end;
+
+    // Inicializar line con un tamaño igual al doble del tamaño del buffer de lectura
+    initLine(&line);
+
+    // Reservar memoria para el buffer de lectura
+    if ((buf = (char *)malloc(BLOCK_SIZE * sizeof(char))) == NULL)
+    {
+        perror("malloc()");
+        exit(EXIT_FAILURE);
+    }
+
+    // Leer del fichero hasta encontrar un salto de línea
+    while ((num_read = read(fd, buf, BLOCK_SIZE)))
+    {
+        // Al encontrar un salto de línea
+        if ((end = (strchr(buf, '\n'))) != NULL)
+        {
+            // Actualizar la cantidad de bytes que pertenecen a la línea de lo que hemos leído en el buffer
+            read_in_line = end - buf;
+            // Hacer que el offset del descriptor de fichero apunte al siguiente carácter tras el salto de línea, para la próxima vez que leamos
+            lseek(fd, read_in_line - num_read, SEEK_CUR);
+            // Insertamos lo leído en el buffer hasta el salto de línea
+            if (insertLine(&line, buf, read_in_line) == -1)
+            {
+                free(buf);
+                freeLine(&line);
+                fprintf(stderr, "Error. Tamaño de línea mayor que 128\n");
+                exit(EXIT_FAILURE);
+            }
+            // Liberar memoria del buffer
+            free(buf);
+            // Devolver line, el proceso principal se encargará de extraer la cadena de caracteres
+            return line;
+        }
+        // Si no se encuentra salto de línea, simplemente insertamos lo leído en el buffer dentro de la línea
+        insertLine(&line, buf, num_read);
+    }
+    // Liberar memoria del buffer
     free(buf);
-    free(aux);
-    free(line);
+    // Devolver line
+    return line;
+}
+
+// Función para ejecutar una línea de comandos
+void ejecutar_linea(char *l)
+{
+    ejecutando++;
+    // Iniciar proceso hijo para procesar la línea leída
+    char *arr[LINE_SIZE];
+    int i;
+    char *p;
+    pid_t pid;
+    switch (pid = fork())
+    {
+    // Fork falla
+    case -1:
+        perror("fork()");
+        exit(EXIT_FAILURE);
+        break;
+    // Fork exitoso, procesamos la línea leída
+    case 0:
+        // Separar el contenido de la línea en un array para la llamada a execvp
+        i = 0;
+        p = strtok(l, " ");
+        while (p != NULL)
+        {
+            arr[i++] = p;
+            p = strtok(NULL, " ");
+        }
+        arr[i] = NULL;
+        execvp(arr[0], arr);
+        perror("execvp()");
+        exit(EXIT_FAILURE);
+
+    default:
+        return;
+    }
 }
